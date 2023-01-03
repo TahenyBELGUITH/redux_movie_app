30879dd3c9367ce1e32afa9740c0297c86b3c11a
Taheny BELGUITH <taheny.belguith@gmail.com>
2023-01-03 17:26:22 +0100

    Revert "Add carousel and search function"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,42 +1,13 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import React from "react";
 import { Link } from "react-router-dom";
-import {
-  fetchAsyncMovies,
-  fetchAsyncShows,
-} from "../../features/movies/movieSlice";
 import user from "../../images/user.png";
-import {FaSearch} from "react-icons/fa";
-import "./Header.scss";
-
+import "./Header.scss"
 const Header = () => {
-  const [term, setTerm] = useState("");
-  const dispatch = useDispatch();
-  const submitHandler = (e) => {
-    e.preventDefault();
-    if (term === "") return alert("Please enter search term!");
-    dispatch(fetchAsyncMovies(term));
-    dispatch(fetchAsyncShows(term));
-    setTerm("");
-  };
   return (
     <div className="header">
-      <div className="logo">
-        <Link to="/">TOuhApp</Link>
-      </div>
-      <div className="search-bar">
-        <form onSubmit={submitHandler}>
-          <input
-            type="text"
-            value={term}
-            placeholder="Search Movies or Shows"
-            onChange={(e) => setTerm(e.target.value)}
-          />
-          <button type="submit">
-            <FaSearch className="fa-search" />
-          </button>
-        </form>
-      </div>
+      <Link to="/">
+        <div className="logo">TOuhApp</div>
+      </Link>
       <div className="user-image">
         <img src={user} alt="user" />
       </div>
